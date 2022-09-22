64c9028d5d3696d1d158a5d2ff2437cb0bc3dfe1
Kimitoshi Takahashi <ktaka@ccmp.jp>
2022-09-22 22:20:02 +0900

    Google OAuth Frontend Simple

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,10 +6,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-      <BrowserRouter>
-	  <App />
-      </BrowserRouter>
-  </React.StrictMode>
+    <React.StrictMode>
+	<BrowserRouter>
+	    <App />
+	</BrowserRouter>
+    </React.StrictMode>
 );
-
