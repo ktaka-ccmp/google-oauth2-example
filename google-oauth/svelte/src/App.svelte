01e9c24a7eaceb4682f0228a4a98e313a4b29d11
Kimitoshi Takahashi <ktaka@ccmp.jp>
2023-12-10 07:31:59 +0900

    Add: svelte for google-auth

---
+++ google-oauth/svelte/src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+  import { Router, Link, Route } from "svelte-routing";
+  import Top from "./cpnts/Top.svelte";
+  import Customer from "./cpnts/Customer.svelte";
+  import NoMatch from "./cpnts/NoMatch.svelte";
+
+  export let url = "";
+</script>
+
+<div class="container-sm">
+  <Router {url}>
+    <nav>
+      <table class="table-borderless table-responsive">
+        <tbody>
+          <tr><td><Link to="/">Top</Link></td></tr>
+          <tr><td><Link to="/customer">Customer</Link></td></tr>
+        </tbody>
+      </table>
+    </nav>
+
+    <div>
+      <Route path="/"><Top /></Route>
+      <Route path="/customer"><Customer /></Route>
+      <Route path="*"><NoMatch /></Route>
+    </div>
+  </Router>
+</div>
+
+<style>
+</style>
---
+++ google-oauth/svelte/src/cpnts/Top.svelte
@@ -0,0 +1,1 @@
+<h2>This is top page.</h2>
