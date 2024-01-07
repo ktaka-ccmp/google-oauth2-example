0356311fb069e1527a623257923df1fa0310a4f5
Kimitoshi Takahashi <ktaka@ccmp.jp>
2024-01-07 23:14:13 +0900

    Chg: Refactor frontend-react02/src/AuthProvider.js

--- google-oauth/frontend-react02/src/AuthProvider.js
+++ google-oauth/frontend-react02/src/AuthProvider.js
@@ -1,80 +1,73 @@
-import { useState, useRef, useMemo, useEffect, useContext, createContext } from 'react';
+import { useState, useMemo, useEffect, useContext, createContext } from 'react';
 import { Navigate, useNavigate, useLocation, Outlet } from 'react-router-dom';
 import axios from 'axios';
 
 export const AuthContext = createContext(null);
 
 export const AuthProvider = ({ children }) => {
-    const navigate = useNavigate();
-    const location = useLocation();
+	const navigate = useNavigate();
+	const location = useLocation();
 
-    const [LoadUser, setLoadUser] = useState(false);
-    const userRef = useRef(null);
+	const [LoadUser, setLoadUser] = useState(false);
+	const [User, setUser] = useState(null);
 
-    const apiAxios = axios.create({
-	baseURL: `${process.env.REACT_APP_API_SERVER}`,
-	withCredentials: true,
-    });
+	const apiAxios = axios.create({
+		baseURL: `${process.env.REACT_APP_API_SERVER}`,
+		withCredentials: true,
+	});
 
-    apiAxios.interceptors.response.use(response => {return response}, error => {
-	if (error.response.status === 401 || error.response.status === 403) {
-	    console.log("apiAxios failed. Redirecting to /login... ")
-	    userRef.current = null;
-		if (location.pathname!=="/login"){
-			navigate('/login', {state: { from: location }}, {replace: true});
-		}
-	}
-	return Promise.reject(error);
-    });
+	const getUser = () => {
+		setLoadUser(true)
+		setUser(null)
+		apiAxios.get(`/api/user/`)
+		.then(res => setUser(res.data))
+		.catch(error => console.log("getUser faild: ", error.response))
+		.finally(() => {
+			setLoadUser(false)
+			console.log("getUser: user:", User)
+			console.log("getUser: location:", location.pathname)
+		})
+	};
 
-    const getUser = () => {
-	setLoadUser(true);
-	apiAxios.get(`/api/user/`)
-	    .then(res => {
-		userRef.current = res.data
-		setLoadUser(false);
-		console.log("getUser: user:", userRef.current.username)
-	    })
-	    .catch(error => console.log("getUser faild: ", error.response))
-    };
-
-    useMemo(() => {
-	getUser();
-	console.log("useMemo: user:", userRef.current?.username)
-	console.log("useMemo: location:", location.pathname)
-    }, [location]);
+	useMemo(() => {
+		getUser();
+	}, [location]);
 
     const backendAuth = (response) => {
-	const origin = location.state?.from?.pathname || '/' ;
-	const data=JSON.stringify(response, null, 2);
-	console.log("response\n", JSON.stringify(response, null, 2));
+		const origin = location.state?.from?.pathname || '/' ;
+		const data=JSON.stringify(response, null, 2);
+		console.log("backendAuth: response\n", JSON.stringify(response, null, 2));
+		console.log("backendAuth: location.state.from\n", location.state.from)
 
-	apiAxios.post(`/api/login/`, data, )
-	    .then(res => {
-		console.log("Backend Auth: ", res)
-		navigate(origin);
-	    })
-	    .catch(error => {
-		console.log(error.response)
-		console.log("backendAuth failed. Redirecting to /login... ")
-		navigate('/login', {state: { from: location }}, {replace: true});
-	    })
-    }
+		apiAxios.post(`/api/login/`, data, )
+		.then(res => {
+			console.log("backendAuth: res.data", res.data)
+			navigate(origin);
+		})
+		.catch(error => {
+			console.log(error.response)
+			console.log("backendAuth failed. Redirecting to /login... ")
+			navigate('/login', {state: { from: location }}, {replace: true});
+		})
+	}
 
-    const handleLogout = () => {
-	userRef.current = null;
-	apiAxios.get(`/api/logout/`)
-	    .then(res => navigate(location))
-	    .catch(error => console.log("Logout failed: ", error))
-    }
+	const handleLogout = () => {
+		apiAxios.get(`/api/logout/`)
+		.catch(error => console.log("Logout failed: ", error))
+		.finally(() => {
+			setUser(null);
+			console.log("handleLogout redirecting to ", location.pathname)
+			navigate(location);
+		})
+	}
 
     const value = {
-	onLogin: backendAuth,
-	onLogout: handleLogout,
-	user: userRef.current,
-	LoadUser,
-	apiAxios,
-    };
+		onLogin: backendAuth,
+		onLogout: handleLogout,
+		user: User,
+		LoadUser,
+		apiAxios,
+	};
 
     return (
 	<AuthContext.Provider value={value} >
@@ -125,16 +118,16 @@ export const UserLogin = () => {
     );
 };
 
-export const RequireAuth = ({ children }) => {
+export const RequireAuth = () => {
     const { LoadUser, user } = useContext(AuthContext);
     const location = useLocation();
 
-    console.log("RequireAuth: LoadUser:", LoadUser, "user:", user?.username)
+    console.log("RequireAuth: user:", user)
 
     if (LoadUser || user) {
-	return <Outlet />
-    }
-
-    console.log("No user object! Redirecting to /login ...")
-    return <Navigate to="/login" replace state={{ from: location }} />
+		return <Outlet />
+    } else {
+		console.log("RequireAuth: Redirecting to /login ...")
+		return <Navigate to="/login" replace state={{ from: location }} />
+	}
 };
