636e7e9e6a7f243094b67dd276c96ca270b8820a
Kimitoshi Takahashi <ktaka@ccmp.jp>
2024-01-08 01:39:57 +0900

    Fix: google-oauth/frontend-svelte/src/components/Customer.svelte

--- google-oauth/frontend-svelte/src/components/Customer.svelte
+++ google-oauth/frontend-svelte/src/components/Customer.svelte
@@ -4,26 +4,24 @@
   import LogoutButton from "./LogoutButton.svelte";
 
   let customers = [];
+  let Loading = true;
 
-  const getCustomers = async () => {
+  onMount(async () => {
     await new Promise((r) => setTimeout(r, 1000));
     apiAxios
       .get(`/api/customer/`)
       .then((res) => (customers = res.data.results))
-      .catch((error) => console.log(error));
-  };
-
-  onMount(() => {
-    getCustomers();
+      .catch((error) => console.log(error))
+      .finally(() => Loading = false);
   });
 </script>
 
 <LogoutButton />
 <h2>This is Customer.</h2>
 
-{#await customers}
+{#if Loading}
   <p>Loading ...</p>
-{:then customers}
+{:else}
   <div class="table-responsive">
     <table class="table table-bordered table-hover table-striped">
       <thead class="table-light">
@@ -44,4 +42,4 @@
       </tbody>
     </table>
   </div>
-{/await}
+{/if}
